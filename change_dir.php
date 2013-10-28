7577521e7d5d44ecaf0f3834d6c9acd06618efbd
sophspir <sophspir@gmail.com>
2013-10-27 21:20:00 -0700

    Stupid mistake this time

--- change_dir.php
+++ change_dir.php
@@ -12,7 +12,7 @@ ftp_login($_SESSION['ftp'], $_SESSION['username'], $_SESSION['password']);
 
 if(isset($_POST['dir'])) {
 	$cur_dir = ftp_pwd($_SESSION['ftp']);
-	@ftp_chdir($_SESSION['ftp'], $cur_dir);
+	@ftp_chdir($_SESSION['ftp'], $_POST['dir']);
 	if($cur_dir !== ftp_pwd($_SESSION['ftp'])) {
 		echo json_dir('dirChange','true');
 		exit(0);
