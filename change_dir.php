fa47cc424811dfd6f48c49a5954cb92980b18a63
sophspir <sophspir@gmail.com>
2013-10-28 20:27:17 -0700

    Fixed strange behavior in change_dir...hopefully

--- change_dir.php
+++ change_dir.php
@@ -11,15 +11,21 @@ $_SESSION['ftp'] = ftp_connect('localhost', 7821);
 ftp_login($_SESSION['ftp'], $_SESSION['username'], $_SESSION['password']);
 
 if(isset($_POST['dir'])) {
+	/*
 	if(strpos($_POST['dir'], '/') !== FALSE)
 		$dir = realpath($_POST['dir']);
 	else
 		$dir = $_POST['dir'];
-	@ftp_chdir($_SESSION['ftp'], $dir);
+	*/
+	@ftp_chdir($_SESSION['ftp'], $_POST['dir']);
+	echo json_dir('dirChange','true');
+	exit(0);
+	/*
 	if($dir === ftp_pwd($_SESSION['ftp'])) {
 		echo json_dir('dirChange','true');
 		exit(0);
 	}
+	*/
 }
 echo json_dir('dirChange','false');
 
