f92bf8268889b2255b66d81d03ce79bde4ad362a
Bill Trok <bill@bill-Lenovo-G560>
2013-12-06 23:15:24 -0800

    Removed debug code that was causing errors in javascript

--- upload/upload_file.php
+++ upload/upload_file.php
@@ -10,7 +10,8 @@ if(!is_user_valid()) {
 if ($_FILES["file"]["error"] > 0)
   {
     // there was an error during the upload print information about it
-  echo "Error: " . $_FILES["file"]["error"] . "<br>";
+    //echo "Error: " . $_FILES["file"]["error"] . "<br>";
+    echo '{"uploadSuccess":false}';
   }
 else
   {
@@ -20,9 +21,9 @@ else
     $upload_file_name = $_FILES["file"]["name"];
     $temp_file = $_FILES["file"]["tmp_name"];
     //echo "hello: ".$_SESSION['username']."<br>";
-    echo "Upload: " . $_FILES["file"]["name"] . "<br>";
+    //echo "Upload: " . $_FILES["file"]["name"] . "<br>";
     //echo "Type: " . $_FILES["file"]["type"] . "<br>";
-    echo "Size: " . ($_FILES["file"]["size"] / 1024) . " kB<br>";
+    //echo "Size: " . ($_FILES["file"]["size"] / 1024) . " kB<br>";
     //echo "Stored in: " . $_FILES["file"]["tmp_name"]."<br>";
     
     $ftp = ftp_connect($_SESSION['host'], $_SESSION['ftp_port']);
@@ -33,19 +34,19 @@ else
     }
 
     $temp_file_handle = fopen($temp_file,'r');
-    echo "Storing in the directory: ".ftp_pwd($ftp)."<br>";
+    //echo "Storing in the directory: ".ftp_pwd($ftp)."<br>";
 
     //We transfer all files in binary all this means is that new os's
     //won't immediately identity text files as such
     $ftp_transfer_mode = FTP_BINARY;
     if(ftp_fput($ftp,$upload_file_name,$temp_file_handle,$ftp_transfer_mode)) {
         error_log("upload succeded");
-        echo "Successfully uploaded file to ftp server"."<br>";
-        return '{"uploadSuccess":true}';
+        //echo "Successfully uploaded file to ftp server"."<br>";
+        echo '{"uploadSuccess":true}';
     } else {
         error_log("upload failed");
-        echo "Error file upload failed"."<br>";
-        return '{"uploadSuccess":false}';
+        //echo "Error file upload failed"."<br>";
+        echo '{"uploadSuccess":false}';
     }
     
   }
